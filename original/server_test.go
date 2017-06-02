ab6ac8b5777a2a80b9e2dc57931b35c8944d04b0
saxsir <saxsir.256+github@gmail.com>
2017-06-02 20:56:00 +0900

    test.dbにつないでhttp responseの確認をするところまでのテスト書いた
    ```
    $ make test
    GIN_MODE=test go test -v
    === RUN   TestTopページが200を返す
    [GIN] 2017/06/02 - 20:56:10 | 200 |     173.861µs | 127.0.0.1 |   GET
    /
    --- PASS: TestTopページが200を返す (0.00s)
    PASS
    ok      github.com/VG-Tech-Dojo/vg-1day-2017/original   0.017s
    ```

--- original/server_test.go
+++ original/server_test.go
@@ -1,13 +1,50 @@
 package main
 
 import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"os"
 	"testing"
 )
 
-func TestSample(t *testing.T) {
-	value := 1
-	expected := 1
-	if value != expected {
-		t.Fatalf("Expected %v, but %v:", expected, value)
+const (
+	dbconf = "dbconfig.yml"
+	env    = "test"
+)
+
+// テスト毎に立ち上げるのは時間がかかるのでテスト全体で共有して使いまわします
+var ts *httptest.Server
+
+func TestMain(m *testing.M) {
+	os.Exit(realMain(m))
+}
+
+func realMain(m *testing.M) int {
+	ts = defaultTestServer()
+	defer func() {
+		ts.Close()
+	}()
+	return m.Run()
+}
+
+func defaultTestServer() *httptest.Server {
+	s := NewServer()
+	if err := s.Init(dbconf, env); err != nil {
+		panic(fmt.Sprintf("failed to init server: %v", err))
+	}
+
+	return httptest.NewServer(s.Engine)
+}
+
+func TestTopページが200を返す(t *testing.T) {
+	resp, err := http.Get(ts.URL + "/")
+	if err != nil {
+		t.Fatalf("get index page failed: %s", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		t.Fatalf("fail to get index page: status code is %d", resp.StatusCode)
 	}
 }
