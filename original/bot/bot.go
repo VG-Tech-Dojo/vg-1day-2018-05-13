ce6491520a7685fcff31d4bed9b07e9710469f5d
saxsir <saxsir.256+github@gmail.com>
2017-06-08 20:13:02 +0900

    processor.processでエラー処理できるように

--- original/bot/bot.go
+++ original/bot/bot.go
@@ -2,6 +2,7 @@ package bot
 
 import (
 	"context"
+	"log"
 
 	"github.com/VG-Tech-Dojo/vg-1day-2017/original/model"
 )
@@ -34,7 +35,13 @@ func (b *Bot) Run(ctx context.Context) {
 			return
 		case m := <-b.in:
 			if b.checker.Check(m) {
-				b.out <- b.processor.Process(m)
+				nm, err := b.processor.Process(m)
+				if err != nil {
+					log.Printf("%s: %#v\n", b.name, err)
+					// selectから抜ける
+					break
+				}
+				b.out <- nm
 			}
 		}
 	}
