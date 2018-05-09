5219d05cd259c06c7d6033c5ffaa3ae6056cb1cf
Gaia-Murata <Soichiro_Murata@voyagegroup.com>
2018-05-09 17:30:40 +0900

    #1 Mission1-1 ユーザ名の表示

--- gaia/model/message.go
+++ gaia/model/message.go
@@ -9,13 +9,15 @@ type Message struct {
 	ID   int64  `json:"id"`
 	Body string `json:"body"`
 	// 1-1. ユーザー名を表示しよう
+	// ユーザ名を構造体に追加
+	UserName string `json:"username"`
 }
 
 // MessagesAll は全てのメッセージを返します
 func MessagesAll(db *sql.DB) ([]*Message, error) {
 
 	// 1-1. ユーザー名を表示しよう
-	rows, err := db.Query(`select id, body from message`)
+	rows, err := db.Query(`select id, body, username from message`)
 	if err != nil {
 		return nil, err
 	}
@@ -25,7 +27,7 @@ func MessagesAll(db *sql.DB) ([]*Message, error) {
 	for rows.Next() {
 		m := &Message{}
 		// 1-1. ユーザー名を表示しよう
-		if err := rows.Scan(&m.ID, &m.Body); err != nil {
+		if err := rows.Scan(&m.ID, &m.Body, &m.UserName); err != nil {
 			return nil, err
 		}
 		ms = append(ms, m)
@@ -42,7 +44,7 @@ func MessageByID(db *sql.DB, id string) (*Message, error) {
 	m := &Message{}
 
 	// 1-1. ユーザー名を表示しよう
-	if err := db.QueryRow(`select id, body from message where id = ?`, id).Scan(&m.ID, &m.Body); err != nil {
+	if err := db.QueryRow(`select id, body, username from message where id = ?`, id).Scan(&m.ID, &m.Body); err != nil {
 		return nil, err
 	}
 
