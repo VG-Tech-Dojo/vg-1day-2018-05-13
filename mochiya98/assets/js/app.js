da8fdef0a4a1e8a3fb0a1f579908eaef1e50fee0
Yukimasa Funaoka <yukimasafunaoka@gmail.com>
2018-05-13 23:10:31 +0900

    ここだけは修正させて下さいzz(2): component(cpmt)の分離

--- mochiya98/assets/js/app.js
+++ mochiya98/assets/js/app.js
@@ -6,40 +6,6 @@
 	 this.type = 0;
   };
 
-  Vue.component('emoji-sel', {
-	props: {
-		emoji: {
-			type:String,
-			required:true,
-		},
-		emojiRadioId: {
-			type:String,
-			required:true,
-		},
-		value: {
-			type:String,
-			required:true,
-		},
-	},
-	template: `
-	<div class="emoji-inp">
-		<input type="radio" name="emojiRadio" :id="emojiRadioId" :value="emoji" v-model="updVal">
-		<label :for="emojiRadioId">{{emoji}}</label>
-	</div>
- `,
-	computed: {
-		updVal: {
-			get() {
-				return this.value
-			},
-			set(val) {
-				this.$emit('input', val)
-			}
-		}
-	}
- });
-
-
 
   const app = new Vue({
     el: '#app',
