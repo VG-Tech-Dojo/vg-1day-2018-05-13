fe4fd4d2e9912223cbac7c13e7474669b46c523e
prog470dev <tshimizudev4@gmail.com>
2018-05-13 12:07:24 +0900

    表示

---
+++ taiki/bot/processor.go
@@ -0,0 +1,80 @@
+package bot
+
+import (
+	"regexp"
+	"strings"
+
+	"fmt"
+
+	"github.com/VG-Tech-Dojo/vg-1day-2018-05-13/taiki/env"
+	"github.com/VG-Tech-Dojo/vg-1day-2018-05-13/taiki/model"
+	"net/url"
+)
+
+const (
+	keywordAPIURLFormat = "https://jlp.yahooapis.jp/KeyphraseService/V1/extract?appid=%s&sentence=%s&output=json"
+)
+
+type (
+	// Processor はmessageを受け取り、投稿用messageを作るインターフェースです
+	Processor interface {
+		Process(message *model.Message) (*model.Message, error)
+	}
+
+	// HelloWorldProcessor は"hello, world!"メッセージを作るprocessorの構造体です
+	HelloWorldProcessor struct{}
+
+	// OmikujiProcessor は"大吉", "吉", "中吉", "小吉", "末吉", "凶"のいずれかをランダムで作るprocessorの構造体です
+	OmikujiProcessor struct{}
+
+	// KeywordProcessor はメッセージ本文からキーワードを抽出するprocessorの構造体です
+	KeywordProcessor struct{}
+)
+
+// Process は"hello, world!"というbodyがセットされたメッセージのポインタを返します
+func (p *HelloWorldProcessor) Process(msgIn *model.Message) (*model.Message, error) {
+	return &model.Message{
+		Body: msgIn.Body + ", world!",
+	}, nil
+}
+
+// Process は"大吉", "吉", "中吉", "小吉", "末吉", "凶"のいずれかがbodyにセットされたメッセージへのポインタを返します
+func (p *OmikujiProcessor) Process(msgIn *model.Message) (*model.Message, error) {
+	fortunes := []string{
+		"大吉",
+		"吉",
+		"中吉",
+		"小吉",
+		"末吉",
+		"凶",
+	}
+	result := fortunes[randIntn(len(fortunes))]
+	return &model.Message{
+		Body: result,
+	}, nil
+}
+
+// Process はメッセージ本文からキーワードを抽出します
+func (p *KeywordProcessor) Process(msgIn *model.Message) (*model.Message, error) {
+	r := regexp.MustCompile("\\Akeyword (.*)\\z")
+	matchedStrings := r.FindStringSubmatch(msgIn.Body)
+	text := matchedStrings[1]
+
+	url := fmt.Sprintf(keywordAPIURLFormat, env.KeywordAPIAppID, url.QueryEscape(text))
+
+	type keywordAPIResponse map[string]interface{}
+	var json keywordAPIResponse
+	get(url, &json)
+
+	keywords := []string{}
+	for k, v := range json {
+		if k == "Error" {
+			return nil, fmt.Errorf("%#v", v)
+		}
+		keywords = append(keywords, k)
+	}
+
+	return &model.Message{
+		Body: "キーワード：" + strings.Join(keywords, ", "),
+	}, nil
+}
