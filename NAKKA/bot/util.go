eaa98cfb794bf480150887c77b0cd3f03ddfc575
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-05-13 17:49:44 +0900

    [WIP] #135 Add twitter get

--- NAKKA/bot/util.go
+++ NAKKA/bot/util.go
@@ -3,12 +3,21 @@ package bot
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
+	"github.com/VG-Tech-Dojo/vg-1day-2018-05-13/NAKKA/env"
+	"github.com/mrjones/oauth"
 	"io/ioutil"
+	"log"
 	"math/rand"
 	"net/http"
 	"net/url"
 	"time"
 )
+
+type Twitter struct {
+	consumer    *oauth.Consumer
+	accessToken *oauth.AccessToken
+}
 
 // get はurlにGETします
 func get(url string, out interface{}) error {
@@ -81,4 +90,56 @@ func postJSON(url string, input interface{}, output interface{}) error {
 func randIntn(n int) int {
 	rand.Seed(time.Now().UnixNano())
 	return rand.Intn(n)
+}
+
+// ------------------------------------------------------
+
+func NewTwitter(consumerKey, consumerSecret, accessToken, accessTokenSecret string) *Twitter {
+	twitter := new(Twitter)
+	twitter.consumer = oauth.NewConsumer(
+		consumerKey,
+		consumerSecret,
+		oauth.ServiceProvider{
+			RequestTokenUrl:   "http://api.twitter.com/oauth/request_token",
+			AuthorizeTokenUrl: "https://api.twitter.com/oauth/authorize",
+			AccessTokenUrl:    "https://api.twitter.com/oauth/access_token",
+		})
+	twitter.accessToken = &oauth.AccessToken{accessToken, accessTokenSecret}
+	return twitter
+}
+
+func (t *Twitter) get(url string, params map[string]string) (interface{}, error) {
+	response, err := t.consumer.Get(url, params, t.accessToken)
+	if err != nil {
+		return nil, err
+	}
+	defer response.Body.Close()
+
+	b, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	// decode
+	var result interface{}
+	err = json.Unmarshal(b, &result)
+	return result, err
+}
+
+func twitterGet(url string) (msg string, err error) {
+	twitter := NewTwitter(env.TwitterConsumerKey, env.TwitterConsumerSecret, env.TwitterAccessToken, env.TwitterTokenSecret)
+
+	// ホームタイムラインを取得
+	res, err := twitter.get(
+		"https://api.twitter.com/1.1/statuses/home_timeline.json", // Resource URL
+		map[string]string{})                                       // Parameters
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, val := range res.([]interface{}) {
+		tweet, _ := val.(map[string]interface{})
+		fmt.Println(tweet["text"])
+	}
+	return "", nil
 }
