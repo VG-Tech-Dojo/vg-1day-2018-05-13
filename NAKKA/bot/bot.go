fa05f79a5b267d7e78d3da67a1e61ee09425ced2
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-05-13 15:12:35 +0900

    #96 Add gacha bot

--- NAKKA/bot/bot.go
+++ NAKKA/bot/bot.go
@@ -100,3 +100,20 @@ func NewKeywordBot(out chan *model.Message) *Bot {
 		processor: processor,
 	}
 }
+
+func NewGachaBot(out chan *model.Message) *Bot {
+	in := make(chan *model.Message)
+
+	checker := NewRegexpChecker("\\Agacha\\z")
+
+	processor := &GachaProcesseor{}
+
+	return &Bot{
+		name:      "gachabot",
+		in:        in,
+		out:       out,
+		checker:   checker,
+		processor: processor,
+	}
+
+}
