ee2551bc086ad3e90c94a861c11072bdb468002b
Yukimasa Funaoka <yukimasafunaoka@gmail.com>
2018-05-13 11:50:19 +0900

    tutorial 1-1

--- mochiya98/model/message.go
+++ mochiya98/model/message.go
@@ -6,16 +6,16 @@ import (
 
 // Message はメッセージの構造体です
 type Message struct {
-	ID   int64  `json:"id"`
-	Body string `json:"body"`
-	// Tutorial 1-1. ユーザー名を表示しよう
+	ID       int64  `json:"id"`
+	Body     string `json:"body"`
+	UserName string `json:"username"`
 }
 
 // MessagesAll は全てのメッセージを返します
 func MessagesAll(db *sql.DB) ([]*Message, error) {
 
 	// Tutorial 1-1. ユーザー名を表示しよう
-	rows, err := db.Query(`select id, body from message`)
+	rows, err := db.Query(`select id, body, username from message`)
 	if err != nil {
 		return nil, err
 	}
@@ -25,7 +25,7 @@ func MessagesAll(db *sql.DB) ([]*Message, error) {
 	for rows.Next() {
 		m := &Message{}
 		// Tutorial 1-1. ユーザー名を表示しよう
-		if err := rows.Scan(&m.ID, &m.Body); err != nil {
+		if err := rows.Scan(&m.ID, &m.Body, &m.UserName); err != nil {
 			return nil, err
 		}
 		ms = append(ms, m)
@@ -42,7 +42,7 @@ func MessageByID(db *sql.DB, id string) (*Message, error) {
 	m := &Message{}
 
 	// Tutorial 1-1. ユーザー名を表示しよう
-	if err := db.QueryRow(`select id, body from message where id = ?`, id).Scan(&m.ID, &m.Body); err != nil {
+	if err := db.QueryRow(`select id, body, username from message where id = ?`, id).Scan(&m.ID, &m.Body, &m.UserName); err != nil {
 		return nil, err
 	}
 
