0005241d9258d69d4c2e0a324f882c3c645080db
Yukimasa Funaoka <yukimasafunaoka@gmail.com>
2018-05-13 23:05:05 +0900

    ここだけは修正させて下さいzz(1): propsの詳細化、キモいネーミングの修正、===への切り替え

--- mochiya98/assets/js/app.js
+++ mochiya98/assets/js/app.js
@@ -7,17 +7,24 @@
   };
 
   Vue.component('emoji-sel', {
-	// Tutorial 1-1. ユーザー名を表示しよう
-	props: ['emoji', 'emojiradioid', 'value'],
-	data() {
-	  return {
-	  }
+	props: {
+		emoji: {
+			type:String,
+			required:true,
+		},
+		emojiRadioId: {
+			type:String,
+			required:true,
+		},
+		value: {
+			type:String,
+			required:true,
+		},
 	},
-	// Tutorial 1-1. ユーザー名を表示しよう
 	template: `
 	<div class="emoji-inp">
-		<input type="radio" name="emojiRadio" :id="emojiradioid" :value="emoji" v-model="updVal">
-		<label :for="emojiradioid">{{emoji}}</label>
+		<input type="radio" name="emojiRadio" :id="emojiRadioId" :value="emoji" v-model="updVal">
+		<label :for="emojiRadioId">{{emoji}}</label>
 	</div>
  `,
 	computed: {
@@ -62,8 +69,16 @@
         });
       },
       sendMessage() {
-        const message = this.newMessage;
-        fetch('/api/messages', {
+		  const message = this.newMessage;
+		  const emojiCharRegEx = 
+			/^[\u{1f300}-\u{1f5ff}\u{1f900}-\u{1f9ff}\u{1f600}-\u{1f64f}\u{1f680}-\u{1f6ff}\u{2600}-\u{26ff}\u{2700}-\u{27bf}\u{1f1e6}-\u{1f1ff}\u{1f191}-\u{1f251}\u{1f004}\u{1f0cf}\u{1f170}-\u{1f171}\u{1f17e}-\u{1f17f}\u{1f18e}\u{3030}\u{2b50}\u{2b55}\u{2934}-\u{2935}\u{2b05}-\u{2b07}\u{2b1b}-\u{2b1c}\u{3297}\u{3299}\u{303d}\u{00a9}\u{00ae}\u{2122}\u{23f3}\u{24c2}\u{23e9}-\u{23ef}\u{25b6}\u{23f8}-\u{23fa}]$/u
+		  console.log(this.newMessage.type);
+			if(this.newMessage.type === 1 && !this.newMessage.body.match(emojiCharRegEx)){
+			  alert("Emojiが選択されていません");
+			  return;
+		  }
+		  return;
+			fetch('/api/messages', {
           method: 'POST',
           body: JSON.stringify(message)
         })
