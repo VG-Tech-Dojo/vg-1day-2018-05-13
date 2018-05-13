3a2cfd3eb529b101375637ea954edc37ba82b1de
Prog24 <prog24.net@gmail.com>
2018-05-13 13:32:20 +0900

    #61 create update message API

--- prog24/controller/message.go
+++ prog24/controller/message.go
@@ -98,7 +98,31 @@ func (m *Message) Create(c *gin.Context) {
 func (m *Message) UpdateByID(c *gin.Context) {
 	// Mission 1-1. メッセージを編集しよう
 	// ...
-	c.JSON(http.StatusCreated, gin.H{})
+	var msg model.Message
+
+	if c.Request.ContentLength == 0 {
+		resp := httputil.NewErrorResponse(errors.New("body is missing"))
+		c.JSON(http.StatusBadRequest, resp)
+		return
+	}
+	if err := c.BindJSON(&msg); err != nil {
+		resp := httputil.NewErrorResponse(err)
+		c.JSON(http.StatusInternalServerError, resp)
+		return
+	}
+	updated, err := msg.Update(m.DB)
+	if err != nil {
+		resp := httputil.NewErrorResponse(err)
+		c.JSON(http.StatusInternalServerError, resp)
+		return
+	}
+
+	m.Stream <- updated
+
+	c.JSON(http.StatusCreated, gin.H{
+		"result": updated,
+		"error": nil,
+	})
 }
 
 // DeleteByID は...
